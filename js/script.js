820baf1c770e325b11bd2c8b9a5ecde44ac737a0
banmala <banmalasunil@gmail.com>
2025-11-21 00:32:40 +0545

    our team section finished

--- js/script.js
+++ js/script.js
@@ -1,16 +1,48 @@
+window.onload=()=>{
+  toggleNavbar();
+  toggleMobileNav();
+}
+
 // Setup Navbar Scroll Effect
-(function setupNavbarScroll() {
+const toggleNavbar = () => {
     window.addEventListener('scroll', () => {
         const navbar = document.getElementById('mainNavbar');
-        console.log("navbar: ", navbar)
         if (window.scrollY > 100) {
             navbar.classList.add('scrolled');
         } else {
             navbar.classList.remove('scrolled');
         }
     });
-})()
+};
 
-(function toggleMobileNav(){
+const toggleMobileNav = () => {
+    const navToggler = document.querySelector('#nav-toggler');
+    const navMenu = document.querySelector('#mobile-menu');
+    const navLinks = document.querySelectorAll('#mobile-menu a');
+    let lastY = 0;
+    // togglerClick function
+    function togglerClick() {
+        if(navMenu.classList.contains('opacity-0')) {
+        navMenu.classList.remove('opacity-0');
+        navMenu.classList.add('opacity-100');
+        }else{
+        navMenu.classList.remove('opacity-100');
+        navMenu.classList.add('opacity-0');
+        }
+    }
+    // navLinkClick function
+    function navLinkClick() {
+        if(navMenu.classList.contains('opacity-100')) {
+            navToggler.click();
+        }
+    }
+    window.addEventListener('scroll', (e) => {
+        if (window.scrollY > 50 && navMenu.classList.contains('opacity-100')) {
+            navMenu.classList.remove('opacity-100');
+            navMenu.classList.add('opacity-0');
+        }
+    });
 
-})()
+    navToggler.addEventListener('click', togglerClick);
+    navLinks.forEach( elem => elem.addEventListener('click', navLinkClick));
+}
